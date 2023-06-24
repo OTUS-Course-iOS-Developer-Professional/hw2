5a476b73462a17b3587f4e19d29f4e5f974a85db
Yulia Popova <juliapopova@jivosite.com>
2023-06-24 20:42:23 +0300

    Initial Commit

---
+++ OTUS_HW2/OTUS_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_HW2App.swift
+//  OTUS_HW2
+//
+//  Created by Yulia on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
